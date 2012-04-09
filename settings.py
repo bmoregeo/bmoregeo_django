93ce73c7b6398a44104cd4f05c0f51b2bd5d50c1
Christopher <christopher.fricke@bmoregeo.com>
2012-04-08 20:02:33 -0400

    Fixed Statistics Model/View
    Added Charts and Table Views

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 gettext = lambda s: s
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -14,9 +14,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django_v1',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, 'database.sqlite'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -117,7 +117,7 @@ MIDDLEWARE_CLASSES = (
 
 )
 
-ROOT_URLCONF = 'v1.urls'
+ROOT_URLCONF = 'bmoregeo_django.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- startServer.sh
+++ startServer.sh
@@ -1,2 +1,2 @@
-python manage.py runserver ec2-50-17-11-115.compute-1.amazonaws.com:8080
-
+#python manage.py runserver ec2-50-17-11-115.compute-1.amazonaws.com:8080
+python manage.py runserver
